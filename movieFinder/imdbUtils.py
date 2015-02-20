6a5dc3681e1b0ae2c2906615e97ad9c41d78f0f0
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 16:29:14 +0200

    ver 1.0

--- movieFinder/imdbUtils.py
+++ movieFinder/imdbUtils.py
@@ -11,10 +11,25 @@ def getImdbByMovieId(movieId):
 	basicUrl = "http://www.omdbapi.com/?i="
 	movieUrl = basicUrl + movieId
 	return urllib.urlopen(movieUrl)
+
+# Returns the movie genre from the given json search result of a movie
+def getMovieGenre(jsonMovie):
+	genreStartIndex = jsonMovie.index("Genre") + 8
+	genreEndIndex = jsonMovie.index("Director") - 3
+	genre = jsonMovie[genreStartIndex:genreEndIndex]
+	return genre	
+
+# Return the movie rating from the given json search result of a movie	
+def getMovieRating(jsonMovie):
+	ratingIndex = jsonMovie.index("imdbRating")
+	return jsonMovie[ratingIndex+13:ratingIndex+16]	
 	
-# Returns the IMDB rating of the given movie name
+	
+# Returns the IMDB rating and genre of the given movie name in (rating,genre) format
+#
 # The 1st IMDB search result (by movie name) is taken into account
-def getMovieRating(movieName):	
+# The rating is on 1-10 scale
+def getMovieRatingAndGenre(movieName):	
 	# Get first movie id 			
 	titleSearchResults = getImdbSearchResults(movieName)
 	searchJson = titleSearchResults.readline()
@@ -31,6 +46,8 @@ def getMovieRating(movieName):
 	# Get movie Rating
 	idSearchResults = getImdbByMovieId(movieId)
 	idSearchJson = idSearchResults.readline()
-	ratingIndex = idSearchJson.index("imdbRating")
-	return idSearchJson[ratingIndex+13:ratingIndex+16]	
+	
+	rating = getMovieRating(idSearchJson)
+	genre = getMovieGenre(idSearchJson)
+	return rating,genre
 	
