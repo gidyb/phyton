f15216ade8674251609728e56d1ef49265c23a35
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 20:13:15 +0200

    ver 2.0

--- movieFinder/movieFinderUtils.py
+++ movieFinder/movieFinderUtils.py
@@ -1,24 +1,25 @@
-import imdbUtils
+import rottenTomatoesUtils
+import movieFinderUtils
 
-# Returns 10-100 scale IMDB rating and genre of the given movie
-# if the movie has no rating yet (it's too new) - return "N/A" for the rating
-def getImdbRatingAndGenre(movie):
+# Top Box Office movies
+#***********************
+topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
+movieFinderUtils.printLabel("Top Box Office Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
 
-	imdbRating, genre = imdbUtils.getMovieRatingAndGenre(movie)
-	# Ignore movies with no IMDB rating 
-	if (imdbRating <> "N/A") :
-		# Convert to 10-100 scale
-		imdbRating = float(imdbRating) * 10
+print
+print
+
+# Top DVD Rentals movies
+#***********************
+topDvdRentalsAndRatings = rottenTomatoesUtils.getTopDvdRentalsMovieNamesAndRatings()
+movieFinderUtils.printLabel("Top DVD Rentals Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
+
 	
-	return imdbRating, genre
-	
-	
-# Returns the average of the given movie ratings, ignoring null ratings
-def getAverageRating(imdbRating, tomatoesRating):
-	# Ignore IMDB null ratings 
-	if (imdbRating == "N/A"):
-		averageRating = tomatoesRating
-	else:
-		averageRating = (imdbRating + tomatoesRating) / 2
-	
-	return int(averageRating)
+# Wait for input so that the cmd window won't close	
+print
+var = raw_input("Press any key to continue...")
+
+
+
