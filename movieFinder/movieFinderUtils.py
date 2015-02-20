f1d8959618bdefe55624e3d9c1cbfd7b6e2a0ab0
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 20:13:41 +0200

    ver 2.0

--- movieFinder/movieFinderUtils.py
+++ movieFinder/movieFinderUtils.py
@@ -1,25 +1,40 @@
-import rottenTomatoesUtils
-import movieFinderUtils
+import imdbUtils
 
-# Top Box Office movies
-#***********************
-topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
-movieFinderUtils.printLabel("Top Box Office Movies:")
-movieFinderUtils.printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
+# Returns 10-100 scale IMDB rating and genre of the given movie
+# if the movie has no rating yet (it's too new) - return "N/A" for the rating
+def getImdbRatingAndGenre(movie):
 
-print
-print
-
-# Top DVD Rentals movies
-#***********************
-topDvdRentalsAndRatings = rottenTomatoesUtils.getTopDvdRentalsMovieNamesAndRatings()
-movieFinderUtils.printLabel("Top DVD Rentals Movies:")
-movieFinderUtils.printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
-
+	imdbRating, genre = imdbUtils.getMovieRatingAndGenre(movie)
+	# Ignore movies with no IMDB rating 
+	if (imdbRating <> "N/A") :
+		# Convert to 10-100 scale
+		imdbRating = float(imdbRating) * 10
 	
-# Wait for input so that the cmd window won't close	
-print
-var = raw_input("Press any key to continue...")
+	return imdbRating, genre
+	
+	
+# Returns the average of the given movie ratings, ignoring null ratings
+def getAverageRating(imdbRating, tomatoesRating):
+	# Ignore IMDB null ratings 
+	if (imdbRating == "N/A"):
+		averageRating = tomatoesRating
+	else:
+		averageRating = (imdbRating + tomatoesRating) / 2
+	
+	return int(averageRating)
+	
 
-
-
+# Prints a list of the given movies, their genre and the average rating between the given
+# RottenTomatoes rating and IMDB rating
+def printMovieNamesAndAverageRatings(movieNamesAndTomatoesRatings):
+	for movie,tomatoesRating in movieNamesAndTomatoesRatings.iteritems():
+		imdbRating, genre = getImdbRatingAndGenre(movie)
+		print movie + "  (" + genre + "),  " + "Rating: ", getAverageRating(imdbRating, tomatoesRating)
+		
+		
+# Prints the given label nicely	
+def printLabel(label):
+	print "**********************"
+	print label
+	print "**********************"
+	print		
