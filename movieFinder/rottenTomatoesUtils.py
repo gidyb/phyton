0b490ae8028bdb0d19be9e39af961ed78c095262
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 20:14:07 +0200

    ver 2.0

--- movieFinder/rottenTomatoesUtils.py
+++ movieFinder/rottenTomatoesUtils.py
@@ -4,9 +4,22 @@ import json
 # Returns the 10 Top US Box Office movie names and their average
 # (critics & audience) ratings
 def getTopBoxOfficeMovieNamesAndRatings():
-	bestMoviesUrl = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=8b22adt8wam7ned65u2nreqq"
-	searchResultsJSON = urllib.urlopen(bestMoviesUrl)
-		
+	bestMoviesURL = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=8b22adt8wam7ned65u2nreqq"
+	return getMovieNamesAndRatingsFromJson(bestMoviesURL)	
+
+
+# Returns the 10 Top US DVD Rentals movie names and their average
+# (critics & audience) ratings
+def getTopDvdRentalsMovieNamesAndRatings():
+	bestDVDsURL = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=8b22adt8wam7ned65u2nreqq"
+	return getMovieNamesAndRatingsFromJson(bestDVDsURL)	
+	
+	
+# Returns the movie names and average ratings from the given RottenTomatoes search URL
+def getMovieNamesAndRatingsFromJson(searchURL):
+
+	searchResultsJSON = urllib.urlopen(searchURL)
+
 	movieNamesRatings = {}
 
 	# Pass empty lines
@@ -32,6 +45,7 @@ def getTopBoxOfficeMovieNamesAndRatings():
 	
 	return movieNamesRatings	
 	
+	
 # Returns the average rating (critics & audience) of the given movie name
 # from the given JSON search result	
 def getAverageMovieRating(movieName, jsonResult):
@@ -46,7 +60,5 @@ def getAverageMovieRating(movieName, jsonResult):
 
 	# Return the average rating
 	averageRating = (criticsRating + audienceRating) / 2
-	return averageRating
-	
-	
+	return averageRating	
 	
