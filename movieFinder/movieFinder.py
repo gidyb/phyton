ad065d0b3b2f0f418bd625b6e96c32acc08b1312
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 20:09:49 +0200

    ver 2.0

--- movieFinder/movieFinder.py
+++ movieFinder/movieFinder.py
@@ -1,21 +1,35 @@
 import rottenTomatoesUtils
 import movieFinderUtils
 
-# Get the names of the current 10 top box office movies from RottenTomatoes
-topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
+def printLabel(label):
+	print "**********************"
+	print label
+	print "**********************"
+	print
 
-# Print a nice label
-print "**********************"
-print "Top Box Office Movies:"
-print "**********************"
+# Prints a list of the given movies, their genre and the average rating between the given
+# RottenTomatoes rating and IMDB rating
+def printMovieNamesAndAverageRatings(movieNamesAndTomatoesRatings):
+	for movie,tomatoesRating in movieNamesAndTomatoesRatings.iteritems():
+		imdbRating, genre = movieFinderUtils.getImdbRatingAndGenre(movie)
+		print movie + "  (" + genre + "),  " + "Rating: ", movieFinderUtils.getAverageRating(imdbRating, tomatoesRating)
+		
+
+
+# Top Box Office movies
+#***********************
+topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
+printLabel("Top Box Office Movies:")
+printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
+
+print
 print
 
-# For each movie, get the genre and the average rating on IMDB and RottenTomatoes
-for movie,tomatoesRating in topBoxOfficeMoviesAndRatings.iteritems():
-
-	imdbRating, genre = movieFinderUtils.getImdbRatingAndGenre(movie)
-	
-	print movie + "  (" + genre + "),  " + "Rating: ", movieFinderUtils.getAverageRating(imdbRating, tomatoesRating)
+# Top DVD Rentals movies
+#***********************
+topDvdRentalsAndRatings = rottenTomatoesUtils.getTopDvdRentalsMovieNamesAndRatings()
+printLabel("Top DVD Rentals Movies:")
+printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
 
 	
 # Wait for input so that the cmd window won't close	
