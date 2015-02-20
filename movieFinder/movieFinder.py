645530244d62b10de4ba469dae4e29e0b05c2752
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 16:30:10 +0200

    ver 1.0

--- movieFinder/movieFinder.py
+++ movieFinder/movieFinder.py
@@ -1,9 +1,26 @@
-import imdbUtils
 import rottenTomatoesUtils
+import movieFinderUtils
 
-topBoxOfficeMovies = rottenTomatoesUtils.getTopBoxOfficeMovies()
+# Get the names of the current 10 top box office movies from RottenTomatoes
+topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
 
-for movie in topBoxOfficeMovies:
+# Print a nice label
+print "**********************"
+print "Top Box Office Movies:"
+print "**********************"
+print
 
-	movieRating = imdbUtils.getMovieRating(movie)
-	print movie, movieRating
+# For each movie, get the genre and the average rating on IMDB and RottenTomatoes
+for movie,tomatoesRating in topBoxOfficeMoviesAndRatings.iteritems():
+
+	imdbRating, genre = movieFinderUtils.getImdbRatingAndGenre(movie)
+	
+	print movie + "  (" + genre + "),  " + "Rating: ", movieFinderUtils.getAverageRating(imdbRating, tomatoesRating)
+
+	
+# Wait for input so that the cmd window won't close	
+print
+var = raw_input("Press any key to continue...")
+
+
+
