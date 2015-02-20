d7a0eaaf364594a770db0b4bd1cdceb23bd9e242
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-20 15:13:03 +0200

    Update imdbUtils.py

--- movieFinder/imdbUtils.py
+++ movieFinder/imdbUtils.py
@@ -1,15 +1,19 @@
 import urllib
 
+# Returns the result of searching IMDB by movie name
 def getImdbSearchResults(movieName):
 	basicUrl = "http://www.omdbapi.com/?s="
 	movieUrl = basicUrl + movieName
 	return urllib.urlopen(movieUrl)
 
+# Returns the result of searching IMDB by movie id
 def getImdbByMovieId(movieId):
 	basicUrl = "http://www.omdbapi.com/?i="
 	movieUrl = basicUrl + movieId
 	return urllib.urlopen(movieUrl)
 	
+# Returns the IMDB rating of the given movie name
+# The 1st IMDB search result (by movie name) is taken into account
 def getMovieRating(movieName):	
 	# Get first movie id 			
 	titleSearchResults = getImdbSearchResults(movieName)
@@ -20,7 +24,7 @@ def getMovieRating(movieName):
 	except ValueError:
 		# Search returned no movies
 		print "There are no movies named" , movieName
-		return -1;
+		return -1
 		
 	movieId = searchJson[firstIdIndex:firstIdIndex+9]
 
