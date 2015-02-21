2e2bd10ac87ece23d81eb9dbf7dc1d36483ca173
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-21 11:14:30 +0200

    ver 2.5

--- movieFinder/movieFinder.py
+++ movieFinder/movieFinder.py
@@ -1,26 +1,11 @@
 import rottenTomatoesUtils
 import movieFinderUtils
 
-def printLabel(label):
-	print "**********************"
-	print label
-	print "**********************"
-	print
-
-# Prints a list of the given movies, their genre and the average rating between the given
-# RottenTomatoes rating and IMDB rating
-def printMovieNamesAndAverageRatings(movieNamesAndTomatoesRatings):
-	for movie,tomatoesRating in movieNamesAndTomatoesRatings.iteritems():
-		imdbRating, genre = movieFinderUtils.getImdbRatingAndGenre(movie)
-		print movie + "  (" + genre + "),  " + "Rating: ", movieFinderUtils.getAverageRating(imdbRating, tomatoesRating)
-		
-
-
 # Top Box Office movies
 #***********************
 topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
-printLabel("Top Box Office Movies:")
-printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
+movieFinderUtils.printLabel("Top Box Office Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
 
 print
 print
@@ -28,13 +13,10 @@ print
 # Top DVD Rentals movies
 #***********************
 topDvdRentalsAndRatings = rottenTomatoesUtils.getTopDvdRentalsMovieNamesAndRatings()
-printLabel("Top DVD Rentals Movies:")
-printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
+movieFinderUtils.printLabel("Top DVD Rentals Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
 
 	
 # Wait for input so that the cmd window won't close	
 print
 var = raw_input("Press any key to continue...")
-
-
-
