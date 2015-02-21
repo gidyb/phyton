fd0bff45565292d5c2f45137b75c7ed7ec9cfc8b
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-21 17:23:42 +0200

    ver 3.0

--- movieFinder/metacriticUtils.py
+++ movieFinder/metacriticUtils.py
@@ -1,22 +1,25 @@
 import unirest
+import urllib
 import json
 
 # Returns the given movie rating from Metacritic site (10-100 scale)
 # Returns "N/A" if the movie was not found
 def getMovieRating(movieName):
-	
-	jsonResponse = unirest.post("https://byroredux-metacritic.p.mashape.com/find/movie",
-		  headers={
-		  	"X-Mashape-Key": "0iV8kx3KKImshC2cnis7clmXp7jKp1hQ6yBjsn2FPx4lkK7rB9",
-			  "Content-Type": "application/x-www-form-urlencoded",
-			  "Accept": "application/json"
-		  },
-		  params={
-			  "retry": 4,
-			  "title": movieName
-		  }
-		)
-	
+	try:
+		jsonResponse = unirest.post("https://byroredux-metacritic.p.mashape.com/find/movie",
+			  headers={
+				"X-Mashape-Key": "0iV8kx3KKImshC2cnis7clmXp7jKp1hQ6yBjsn2FPx4lkK7rB9",
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Accept": "application/json"
+			  },
+			  params={
+				"retry": 4,
+				"title": movieName
+			  }
+			)
+	except urllib.URLError:
+		# Timeout from metacritic
+		return "N/A"
 	
 	result = jsonResponse.body['result']
 	
