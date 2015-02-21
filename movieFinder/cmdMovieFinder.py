91171404bf7c8377e0fa2ffee6707888e8757870
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-21 17:30:13 +0200

    ver 3.0

---
+++ movieFinder/cmdMovieFinder.py
@@ -0,0 +1,25 @@
+#
+# This is the simpler, CMD version of the movie finder, used for DEBUG
+#
+import rottenTomatoesUtils
+import movieFinderUtils
+
+# Top Box Office movies
+#***********************
+topBoxOfficeMoviesAndRatings = rottenTomatoesUtils.getTopBoxOfficeMovieNamesAndRatings()
+movieFinderUtils.printLabel("Top Box Office Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topBoxOfficeMoviesAndRatings)
+
+print
+print
+
+# Top DVD Rentals movies
+#***********************
+topDvdRentalsAndRatings = rottenTomatoesUtils.getTopDvdRentalsMovieNamesAndRatings()
+movieFinderUtils.printLabel("Top DVD Rentals Movies:")
+movieFinderUtils.printMovieNamesAndAverageRatings(topDvdRentalsAndRatings)
+
+	
+# Wait for input so that the cmd window won't close	
+print
+var = raw_input("Press any key to continue...")
