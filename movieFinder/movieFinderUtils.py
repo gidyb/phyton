013320a457f071ffe54664f608ef517501c52f70
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-21 11:14:53 +0200

    ver 2.5

--- movieFinder/movieFinderUtils.py
+++ movieFinder/movieFinderUtils.py
@@ -1,35 +1,34 @@
 import imdbUtils
+import metacriticUtils
 
-# Returns 10-100 scale IMDB rating and genre of the given movie
-# if the movie has no rating yet (it's too new) - return "N/A" for the rating
-def getImdbRatingAndGenre(movie):
-
-	imdbRating, genre = imdbUtils.getMovieRatingAndGenre(movie)
-	# Ignore movies with no IMDB rating 
-	if (imdbRating <> "N/A") :
-		# Convert to 10-100 scale
-		imdbRating = float(imdbRating) * 10
-	
-	return imdbRating, genre
-	
-	
 # Returns the average of the given movie ratings, ignoring null ratings
-def getAverageRating(imdbRating, tomatoesRating):
-	# Ignore IMDB null ratings 
-	if (imdbRating == "N/A"):
-		averageRating = tomatoesRating
-	else:
-		averageRating = (imdbRating + tomatoesRating) / 2
+def getAverageRating(imdbRating, tomatoesRating, metacriticRating):
+
+	# Tomatoes rating always exists because we take the movie names from there
+	validRatings = [tomatoesRating]
+		
+	# Add non-null ratings to the ratings list
+	if (imdbRating <> "N/A"):
+		validRatings.append(imdbRating)
 	
-	return int(averageRating)
+	if (metacriticRating <> "N/A"):
+		validRatings.append(metacriticRating)
+	
+	return int(sum(validRatings)/len(validRatings))
 	
 
 # Prints a list of the given movies, their genre and the average rating between the given
 # RottenTomatoes rating and IMDB rating
 def printMovieNamesAndAverageRatings(movieNamesAndTomatoesRatings):
 	for movie,tomatoesRating in movieNamesAndTomatoesRatings.iteritems():
-		imdbRating, genre = getImdbRatingAndGenre(movie)
-		print movie + "  (" + genre + "),  " + "Rating: ", getAverageRating(imdbRating, tomatoesRating)
+		imdbRating, genre = imdbUtils.getMovieRatingAndGenre(movie)
+		metacriticRating = metacriticUtils.getMovieRating(movie)
+		averageRating = getAverageRating(imdbRating, tomatoesRating, metacriticRating)
+		
+		# DEBUG PRINT - all ratings
+		# print movie + "  (" + genre + "),  " + "Imdb: ", imdbRating , " Tomato: ", tomatoesRating, " Meta: ", metacriticRating, " Average:", averageRating
+		
+		print movie + "  (" + genre + "),  " + "Rating: ", averageRating
 		
 		
 # Prints the given label nicely	
