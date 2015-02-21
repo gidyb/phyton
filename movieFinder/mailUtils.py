5b89bc285837c0ddee75665eff4f0e5996d96e09
Gidy Basin <gidyb@users.noreply.github.com>
2015-02-21 17:30:58 +0200

    ver 3.0

---
+++ movieFinder/mailUtils.py
@@ -0,0 +1,36 @@
+import smtplib
+
+class GmailSender(object):
+    def __init__(self):
+        self.email = "gidysmoviemaster@gmail.com"
+        self.password = "moviemaster123"
+        self.server = 'smtp.gmail.com'
+        self.port = 587
+        session = smtplib.SMTP(self.server, self.port)        
+        session.ehlo()
+        session.starttls()
+        session.ehlo
+        session.login(self.email, self.password)
+        self.session = session
+
+    def send_message(self, addressTo, subject, body):
+        ''' This must be removed '''
+        headers = [
+            "From: " + self.email,
+            "Subject: " + subject,
+            "To: " + addressTo,
+            "MIME-Version: 1.0",
+           "Content-Type: text/plain"]
+        headers = "\r\n".join(headers)
+        self.session.sendmail(
+            self.email,
+            addressTo,
+            headers + "\r\n\r\n" + body)
+
+def getMailingList():
+	mailingList = {}
+	
+	mailingList["Gidy"] = "gidy.basin@gmail.com"
+	# More users....
+	
+	return mailingList
