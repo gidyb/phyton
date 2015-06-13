d3ffa200c889cc7416be3646add84acabc0850e5
gidy <gidy@GidyPC>
2015-06-13 18:56:45 +0300

    Ver 4.2

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Movie Finder
 
-This is a script that periodicaly searches for top movies in RottenTomatoes and sends an email with their names, genres and average ratings from various movie ratings sites (IMDB, RottenTomatoes etc.)
+ver 1.0 - Searches for 10 Top Box Office movies in RottenTomatoes and prints their names and their average rating from 
+          RottenTomatoes and IMDB
+          
+ver 2.0 - Top DVD Rentals added - now 2 lists are printed - top box office and top dvd rentals. In addition, the movie 
+          genre is printed
+          
+ver 2.5 - Metacritic site ratings added. All ratings use JSON formatting
+          
+ver 3.0 - The script runs periodically and sends an email with all the current top movies and their ratings to a given mailing list         
 
-ver 3.0 - Movie Finder is constantly running on an Amazon EC2 instance and sends mails to the given mailing list once a week.
+ver 4.0 - The script also looks for movie torrents in yts.to for the listed movies
 
-ver 4.0 - Movie Finder is also searching for torrents to the movies it finds on yts.to. A link to the torrent with the best quality (720p, 1080p etc.) is added to the mail, if such torrent exist. Usually, torrents can be found only for top DVD movies and to the top movies in cinema.
+ver 4.1 - The mailing list is read from a json file
+
+ver 4.2 - Nice HTML e-mails including the torrent as a link, and a link to the IMDB page of the movie 
+
+
+# Requirements
+Unirest lib - for easier http get requests - http://unirest.io/python.html
--- movieFinder/movieFinderUtils.py
+++ movieFinder/movieFinderUtils.py
@@ -22,16 +22,8 @@ def getAverageRating(imdbRating, tomatoesRating, metacriticRating):
 	
 	return int(sum(validRatings)/len(validRatings))
 	
-# Prints the given label nicely	to the cmd
-def printLabel(label):
-	print "**********************"
-	print label
-	print "**********************"
-	print		
-	
-
-# Returns a list of the given movies, their genre and the average rating between various sites
-# Return format is a list of {"movieName":XXX,"genre":GGG,"year":YYYY,"averageRating":RR} objects
+# Returns a list of various params for the given movies
+# Return format is a list of {"movieName":XXX,"genre":GGG,"year":YYYY,"averageRating":RR,"imdbLink":LL} objects
 def	getMovieNamesAndAverageRatings(tomatoMoviesInfo):
 
 	moviesList = []
@@ -41,14 +33,14 @@ def	getMovieNamesAndAverageRatings(tomatoMoviesInfo):
 		movieTomatoRating = movieInfo["movieRating"]
 		movieYear = movieInfo["movieYear"]
 		
-		imdbRating, genre = imdbUtils.getMovieRatingAndGenre(movieName)
+		imdbRating, genre, imdbLink = imdbUtils.getMovieRatingAndGenre(movieName)
 		metacriticRating = metacriticUtils.getMovieRating(movieName)
 		averageRating = getAverageRating(imdbRating, movieTomatoRating, metacriticRating)
 		
 		# DEBUG PRINT - all ratings
 		print movieName + "  (" + genre + "," + str(movieYear) + "),  " + "Imdb: ", imdbRating , " Tomato: ", movieTomatoRating, " MetaCritic: ", metacriticRating, " Average:", str(averageRating)
 	
-		curMovie = {"movieName":movieName,"genre":genre,"year":movieYear,"averageRating":averageRating}
+		curMovie = {"movieName":movieName,"genre":genre,"year":movieYear,"averageRating":averageRating,"imdbLink":imdbLink}
 		moviesList.append(curMovie)
 		
 	return moviesList
@@ -57,10 +49,13 @@ def	getMovieNamesAndAverageRatings(tomatoMoviesInfo):
 # Returns an email version of the given movies list, which include movie names and their ratings
 def getEmailFormatList(moviesList):
 	emailMsg = ""
+	count = 1
 
 	for movieInfo in moviesList:
 
-		movieLine = movieInfo["movieName"] + " (" + movieInfo["genre"] + ")," + tab + "Average Rating: " + str(movieInfo["averageRating"])
+		movieLink = "<a href=\"" + movieInfo["imdbLink"] + "\">" + movieInfo["movieName"] + "</a>"
+		
+		movieLine = str(count) + ") " + movieLink + " (" + movieInfo["genre"] + ")," + tab + "Average Rating: " + str(movieInfo["averageRating"])
 
 		# Look for a torrent link, and add it if found
 		torrentLink = ytsUtils.getTorrentLink(movieInfo)
@@ -68,6 +63,8 @@ def getEmailFormatList(moviesList):
 			movieLine += tab + "<a href=\"" + torrentLink + "\">torrent</a>"	
 		
 		emailMsg += movieLine + newLine
+		
+		count += 1
 		
 	return emailMsg	
 
